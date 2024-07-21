d49d6b879cfba80e334ab3df91078c2709bc5a40
Joao Henrique Alencar <jhbfa2004@gmail.com>
2024-07-20 22:13:09 -0300

    commit final, codigo pronto e devidamente comentado

--- mips_project01.asm
+++ mips_project01.asm
@@ -22,22 +22,22 @@
 	
 	sistema:	# praticamente a funcao principal, ira se repetir
 	
-		la 	$a0, menu	# atribui o menu no registrador
-		jal 	imprimeString	# imprime o menu
-			
-		jal 	leInteiro	# le o retorno do usuario das opcoes do menu
+		la 	$a0, menu		# atribui o menu no registrador
+		jal 	imprimeString		# imprime o menu
+				
+		jal 	leInteiro		# le o retorno do usuario das opcoes do menu
 		
-		move	$s0, $v0	# guarda o retorno do usuário
+		move	$s0, $v0		# guarda o retorno do usuário
 		
 		# usa a escolha do usuario para chamar a funcao correspondente
-		# testa igualdade com branch on equal
+		# testa igualdade com beq ( branch on equal )
     		beq   	$s0, 1, converte	# se 1, chama converte          
     		beq   	$s0, 2, fibonacci	# se 2, chama fibonacci
-    		#beq   	$s0, 3, 
+    		beq   	$s0, 3, enesimoPar	# se 3, chama enesimoPar
     		beq  	$s0, 4, fechaPrograma	# se 4, fecha o programa
     		
 		# qualquer outro valor...
-		j 	sistema 	# volta pro início
+		j 	sistema 		# volta pro início
 	
 	## BLOCO DE FUNCOES DE UTILIDADE GERAL
 	leInteiro:
@@ -91,19 +91,19 @@
 	corrigeFloat:
 		## função para correção de int pra float
 		
-		mtc1	$t0, $f1	# move para o coprocessador 1 (FPU) para movermos para o f1 como float
-		cvt.s.w $f1, $f1	# converter word para precisao unica (evitar dizimas quebradas)
+		mtc1	$t0, $f1	# move para o coprocessador 1 (FPU) para movermos o t0 para o f1 como float
+		cvt.s.w $f1, $f1	# converter o valor (word) para precisao unica (evitar dizimas quebradas)
 		
 		jr 	$ra		# retorna o valor
 		
 	arredonda:
 		## função para arredondar nossos pontos flutuantes com 1 casa decimal
 		
-		l.s $f1, val_10          # carrega 10.0 em $f1
-    		mul.s $f2, $f0, $f1      # multiplica o valor em f0 por 10
-    		round.w.s $f2, $f2       # arredonda o valor multiplcado para o inteiro mais proximo
-    		cvt.s.w $f2, $f2         # converte o inteiro de volta para float
-    		div.s $f0, $f2, $f1      # divide por 10 de novo (assim so terá uma casa decimal)
+		l.s $f1, val_10         # carrega 10.0 em f1
+    		mul.s $f2, $f0, $f1     # multiplica o valor em f0 por 10
+    		round.w.s $f2, $f2      # arredonda o valor multiplcado para o inteiro mais proximo
+    		cvt.s.w $f2, $f2        # converte o inteiro de volta para float
+    		div.s $f0, $f2, $f1	# divide por 10 de novo (assim so terá uma casa decimal)
 		
 		jr	$ra		# retorna o valor
 		
@@ -119,6 +119,7 @@
 
 	encerraSubPrograma:
 		## função criada para encapsular a parte final dos subprogramas, evitando repeticao de codigo
+		
 		jal 	pulaLinha	# pular linhas pra organizar
 		jal	leString	# organizar aguardando valor
 		jal 	pulaLinha	# pular linhas pra organizar
@@ -127,6 +128,7 @@
 	 	
 	fechaPrograma:
 		## função criada apenas para encerrar o programa
+		
 		la 	$a0, encerrando	# atribui o texto de encerramento ao registrador
 		jal 	imprimeString 	# imprime
 		
@@ -136,10 +138,10 @@
 
 	## BLOCO DE FUNCOES ESPECIFICAS
 	converte:
-		la	$a0, entrada_conversor	# carrega o endereço do texto no registrador
+		la	$a0, entrada_conversor	# carrega o endereço do texto de entrada no registrador
 		jal	imprimeString		# imprime o texto
 		
-		jal	leFloat			# recege input do usuario
+		jal	leFloat			# recebe input do usuario
 		
 		aplicaFormula:	# organização
 			li	$t0, 32			# guarda o valor 32 em t0
@@ -147,12 +149,12 @@
 			sub.s	$f0, $f0, $f1		# f0 recebe o valor subtraido por 32
 		
 			li	$t0, 5			# guarda o valor 5 em t0
-			jal	corrigeFloat
+			jal	corrigeFloat		# coloca nosso valor inteiro como aceitavel para operacao float em f1
 			mul.s	$f0, $f0, $f1		# multiplica o valor em f0 por 5	
 				
 			li	$t0, 9			# guarda o valor 9 em t0
-			jal	corrigeFloat
-			div.s	$f0, $f0, $f1		# divide o valor em s0 por 9
+			jal	corrigeFloat		# coloca nosso valor inteiro como aceitavel para operacao float em f1
+			div.s	$f0, $f0, $f1		# divide o valor em f0 por 9
 			
 			jal 	arredonda		# funcao para arredondar e melhorar exibição
 			
@@ -171,19 +173,19 @@
 		jal	encerraSubPrograma	
 	
 	fibonacci:
-		la  	$a0, entrada_fibonacci	# carrega o endereço do texto no registrador
-		jal	imprimeString	# imprime o texto
+		la  	$a0, entrada_fibonacci	# carrega o endereço do texto de entrada no registrador
+		jal	imprimeString		# imprime o texto
 	
-		jal	leInteiro	# recebe input do usuário
-		move	$s0, $v0	# guarda input do usuario no s0
+		jal	leInteiro		# recebe input do usuário
+		move	$s0, $v0		# guarda input do usuario no s0
 	
 		## definir valores iniciais (ambos são 1)
 		li	$s1, 1
 		li	$s2, 1
 	
 		## se o input for 1 ou 2, sempre vai ser 1 o resultado
-		beq	$s0, 1, encerraFibonacci
-		beq	$s0, 2, encerraFibonacci
+		beq	$s0, 1, encerraFibonacci	# pulamos para o encerramento
+		beq	$s0, 2, encerraFibonacci	# pulamos para o encerramento
 		
 		## senao...
 		## será feito um loop n-2 (ja usamos 2 valores acima) vezes para chegar ao valor final
@@ -193,7 +195,8 @@
 			move	$s1, $s2		# s1 recebe o valor de s2
 			move	$s2, $s3		# s2 recebe o valor de s3
 			addi	$s0, $s0, -1		# decrementa o s0
-			bgtz 	$s0, geraProximoValor	# instrucao que verifica nosso contador para saber se é maior que 0
+			bgtz  	$s0, geraProximoValor	# instrucao que verifica nosso contador para saber se é maior que 0
+							 # bgtz = segue o caminho se for maior que zero
 	
 		encerraFibonacci:
 			# assim, o valor correto fica em $s2 sempre
@@ -205,4 +208,30 @@
 			move	$a0, $s2	# coloca no registrador o nosso valor
 			jal 	imprimeInteiro	# imprime noso valor
 			
+		jal	encerraSubPrograma
+		
+	enesimoPar:
+		la  	$a0, entrada_par	# carrega o endereço do texto de entrada no registrador
+		jal	imprimeString		# imprime o texto
+	
+		jal	leInteiro		# recebe input do usuário
+		move	$s0, $v0		# guarda input do usuario no s0
+		
+		geraTermo: 	# organização
+			li	$s1, 2			# guarda 2 no registrador s1
+			mult  	$s0, $s1		# multiplica o input por 2 pra gerar o enesimo termo
+			mflo	$s0			# move o valor que fica no LO para nosso registrador
+							 # esse valor fica no LO nessas multiplicaoes pois sao o LOwer bits,
+							 # menos significativos
+
+		finalizaTermo:	# organização
+		
+			## printa frase final
+			la	$a0, saida_fibonacci
+			jal 	imprimeString
+		
+			move	$a0, $s0	# coloca no registrador o nosso valor
+			jal 	imprimeInteiro	# imprime nosso valor
+		
+	
 		jal	encerraSubPrograma
